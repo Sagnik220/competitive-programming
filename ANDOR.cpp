bd428acbce589d7e39511ffd1099d5f17e9a763f
Sagnik Mukherjee <sagnik2207@gmail.com>
2020-11-01 00:40:36 +0530

    October Lunchtime

---
+++ ANDOR.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() 
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll x;
+        cin>>x;
+        cout<<x<<" "<<0<<endl;
+    }
+	
+	return 0;
+}

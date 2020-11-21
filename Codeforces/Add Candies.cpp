67ae0a15f0e1e0eef35c4b65907341d4c053f1a2
Sagnik Mukherjee <sagnik2207@gmail.com>
2020-11-21 18:58:57 +0530

    Create Add Candies.cpp

---
+++ Codeforces/Add Candies.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        cout<<n<<endl;
+        for(ll i=1;i<=n;i++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+

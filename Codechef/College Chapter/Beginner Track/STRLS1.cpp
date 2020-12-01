4e467e30750305b6133d29430621d8f7006e6c28
Sagnik Mukherjee <sagnik2207@gmail.com>
2020-12-01 21:49:44 +0530

    Create STRLS1.cpp

---
+++ Codechef/College Chapter/Beginner Track/STRLS1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n;
+    cin>>n;
+    string s,o;
+    cin>>s;
+    ll len=s.size();
+    if(len==n)
+    {
+        cout<<s<<endl;
+    }
+    return 0;
+}
+ 

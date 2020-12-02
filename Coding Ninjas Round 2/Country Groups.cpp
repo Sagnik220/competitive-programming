949fac666da4d3f9d7e483c82214a11c3848e5e8
Sagnik Mukherjee <sagnik2207@gmail.com>
2020-12-02 20:13:24 +0530

    Partial Solution

---
+++ Coding Ninjas Round 2/Country Groups.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    cout.tie(NULL);
+    int n,p;
+    cin>>n>>p;
+    for(ll i=0;i<p;i++)
+    {
+        ll x,y;
+        cin>>x>>y;
+    }
+    cout<<p-1<<endl;
+    
+    
+    return 0;
+}

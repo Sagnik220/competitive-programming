81b81d1e547f541b8efcc5c973f4b9829e531a71
Sagnik Mukherjee <sagnik2207@gmail.com>
2021-01-06 19:29:45 +0530

    Create Connectedcomponents.cpp

---
+++ DSA/Connectedcomponents.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+vector<int>ar[100001];
+int vis[100001];
+ 
+void dfs(int node)
+{
+	vis[node] = 1;
+ 
+	for(int child : ar[node])
+	{
+		if(!vis[child])
+		dfs(child);
+	}
+
+}
+ 
+ 
+int main()
+{
+	int n , m , a , b;
+	cin>>n>>m;
+ 
+	for(int i=1;i<=m;i++)
+	{
+		cin>>a>>b;
+		ar[a].push_back(b); 
+		ar[b].push_back(a);
+	}
+ 
+	int cc = 0;
+ 
+	for(int i=1;i<=n;i++)
+	{
+		if(!vis[i])
+		{
+			dfs(i);
+			cc++;
+		}
+	}
+ 
+	cout<<cc;
+}
